42240a1164866efdc54fea6154fd1d7c18ba6377
lk <514531536@qq.com>
2023-02-09 21:55:03 +0800

    feat: 第一种调试模式 pnpm link --global

--- packages/react/src/jsx.ts
+++ packages/react/src/jsx.ts
@@ -62,31 +62,28 @@ export const jsx = (
 	return ReactElement(type, key, ref, props);
 };
 
-export const jsxDEV = (
-  type: ElementType,
-  config: any,
-): ReactElementType => {
-  let key: Key = null;
-  let ref: Ref = null;
-  const props: Props = {};
-  for (const prop in config) {
-    const val = config[prop];
-    if (prop === 'key') {
-      if (val !== undefined) {
-        key = '' + val;
-      }
-      continue;
-    }
-    if (prop === 'ref') {
-      if (val !== undefined) {
-        ref = val;
-      }
-      continue;
-    }
-    if ({}.hasOwnProperty.call(config, prop)) {
-      props[prop] = val;
-    }
-  }
+export const jsxDEV = (type: ElementType, config: any): ReactElementType => {
+	let key: Key = null;
+	let ref: Ref = null;
+	const props: Props = {};
+	for (const prop in config) {
+		const val = config[prop];
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = '' + val;
+			}
+			continue;
+		}
+		if (prop === 'ref') {
+			if (val !== undefined) {
+				ref = val;
+			}
+			continue;
+		}
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = val;
+		}
+	}
 
-  return ReactElement(type, key, ref, props);
+	return ReactElement(type, key, ref, props);
 };
